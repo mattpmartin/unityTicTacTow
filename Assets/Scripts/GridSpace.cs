1976a9ab989815fb2a494a024c4d17cc46451d22
matthewmartin <matt@mattmartin.xyz>
2016-10-30 17:08:37 +1000

    End of tutorial 1 page 4

---
+++ Assets/Scripts/GridSpace.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class GridSpace : MonoBehaviour {
+	public Button button;
+	public Text buttonText;
+	public string playerSide;
+
+	public void SetSpace ()
+	{
+		buttonText.text = playerSide;
+		button.interactable = false;
+	}
+}
