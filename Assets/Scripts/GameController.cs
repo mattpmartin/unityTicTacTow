52db0892b43cae4db2a91a327ccb7ecc2bfbe6cf
matthewmartin <matt@mattmartin.xyz>
2016-10-30 17:40:55 +1000

    End of tutorial 1 page 5

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+	public Text[] buttonList;
+
+	void SetGameControllersOnButtons (){
+		for (int i = 0; i < buttonList.Length; i++) {
+			buttonList [i].GetComponentInParent<GridSpace> ().SetGameControllerReference (this);
+		}
+	}
+
+	void Awake (){
+		SetGameControllersOnButtons ();
+	}
+
+	public string GetPlayerSide (){
+		return "?";
+	}
+
+	public void EndTurn (){
+		Debug.Log("EndTurn is not implemented!");
+	}
+}
--- Assets/Scripts/GridSpace.cs
+++ Assets/Scripts/GridSpace.cs
@@ -4,11 +4,16 @@ using UnityEngine.UI;
 public class GridSpace : MonoBehaviour {
 	public Button button;
 	public Text buttonText;
-	public string playerSide;
+	private GameController gameController;
 
 	public void SetSpace ()
 	{
-		buttonText.text = playerSide;
+		buttonText.text = gameController.GetPlayerSide();;
 		button.interactable = false;
+		gameController.EndTurn();
+	}
+
+	public void SetGameControllerReference (GameController controller){
+		gameController = controller;
 	}
 }
